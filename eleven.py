d35fab9c0123abe2bfb0549bc62a309246148b81
nathanjzhao <43712744+nathanjzhao@users.noreply.github.com>
2024-09-13 21:18:10 -0400

    initial commit

---
+++ eleven.py
@@ -0,0 +1,16 @@
+"""utilize https://elevenlabs.io/ to setup speech synthesis @ansh"""
+
+
+def text_to_speech(text):
+  """
+  text: str which is our model response
+  returns: audio stream with elevenlabs
+  """
+  pass
+
+# def speech_to_speech(audio_stream):
+#   """
+#   audio_stream: audio stream from twilio
+#   returns: audio stream with elevenlabs
+#   """
+#   pass
